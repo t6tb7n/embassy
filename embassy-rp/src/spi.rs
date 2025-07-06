21fb3b01e32e076d564f2b827a3b7e613eb603c4
t6tb7n <t6tb7n@gmail.com>
2025-07-06 06:18:32 +0200

    Add CS to SPI

--- embassy-rp/src/spi.rs
+++ embassy-rp/src/spi.rs
@@ -340,17 +340,18 @@ impl<'d, T: Instance> Spi<'d, T, Async> {
         clk: impl Peripheral<P = impl ClkPin<T> + 'd> + 'd,
         mosi: impl Peripheral<P = impl MosiPin<T> + 'd> + 'd,
         miso: impl Peripheral<P = impl MisoPin<T> + 'd> + 'd,
+        cs: impl Peripheral<P = impl CsPin<T> + 'd> + 'd,
         tx_dma: impl Peripheral<P = impl Channel> + 'd,
         rx_dma: impl Peripheral<P = impl Channel> + 'd,
         config: Config,
     ) -> Self {
-        into_ref!(tx_dma, rx_dma, clk, mosi, miso);
+        into_ref!(tx_dma, rx_dma, clk, mosi, miso, cs);
         Self::new_inner(
             inner,
             Some(clk.map_into()),
             Some(mosi.map_into()),
             Some(miso.map_into()),
-            None,
+            Some(cs.map_into()),
             Some(tx_dma.map_into()),
             Some(rx_dma.map_into()),
             config,
